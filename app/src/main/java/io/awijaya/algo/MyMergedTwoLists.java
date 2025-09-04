8f31ffdb066d91af398f52ad9d7a6c84b64b8387
androwijaya <androwijaya@gmail.com>
2025-09-04 20:05:16 +1000

    added MyRemoveElement algo test

--- app/src/main/java/io/awijaya/algo/MyMergedTwoLists.java
+++ app/src/main/java/io/awijaya/algo/MyMergedTwoLists.java
@@ -1,7 +1,6 @@
 package io.awijaya.algo;
 
 
-
 public class MyMergedTwoLists {
     class ListNode {
         int val;
@@ -18,5 +17,53 @@ public class MyMergedTwoLists {
             this.val = val;
             this.next = next;
         }
+    }
+
+    /**
+     * Approach 1: Iterative Merge with Dummy Node
+     * <p>
+     * This is the most efficient and commonly used approach for merging sorted linked lists.
+     * <p>
+     * Algorithm: 1. Create a dummy node to simplify edge cases 2. Use a pointer to build the
+     * merged list 3. Compare nodes from both lists and link the smaller one 4. Move the pointer
+     * forward 5. Attach remaining nodes from the non-empty list
+     * <p>
+     * Time Complexity: O(n + m) where n and m are the lengths of the two lists Space Complexity:
+     * O(1) as we reuse existing nodes
+     */
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        ListNode dummy = new ListNode(0);
+        ListNode current = dummy;
+
+        while (list1 != null && list2 != null) {
+            if (list1.val <= list2.val) {
+                ListNode tmp = new ListNode(list1.val);
+                current.next = tmp;
+                list1 = list1.next;
+            } else {
+                ListNode tmp = new ListNode(list2.val);
+                current.next = tmp;
+                list2 = list2.next;
+            }
+            current = current.next;
+        }
+
+        if (list1 != null) {
+            current.next = list1;
+        }
+        if (list2 != null) {
+            current.next = list2;
+        }
+
+        return dummy.next;
+    }
+
+    public static void main(String[] args) {
+        int[] list1 = {5};
+        int[] list2 = {1, 2, 4};
+
+        int[] list3 = {1, 2, 4, 5};
+        int[] list4 = {1, 3, 4};
+        int[] output34 = {1, 1, 2, 3, 4, 4, 5};
     }
 }
---
+++ app/src/main/java/io/awijaya/algo/MyRemoveElement.java
@@ -0,0 +1,45 @@
+package io.awijaya.algo;
+
+// https://leetcode.com/problems/remove-element/
+
+public class MyRemoveElement {
+
+    // first successful own implementation, with beats 100% in LeetCode
+    public static int removeElement(int[] nums, int val) {
+
+        for (int i = 0; i < nums.length; i++) {
+            int current = nums[i];
+            if (current == val) {
+                int j = i + 1;
+                while (j < nums.length) {
+                    int next = nums[j];
+                    if (next != val) { // swap only if next is not same as val
+                        nums[i] = next;
+                        nums[j] = current;
+                        break;
+                    }
+                    j++;
+                }
+            }
+        }
+
+        int k = 0;
+        for (int i = 0; i < nums.length; i++) {
+            int current = nums[i];
+            if (current != val) {
+                k++;
+            }
+        }
+
+        return k;
+    }
+
+    public static void main(String[] args) {
+        int[] test1 = {3, 2, 2, 3};
+        System.out.println(removeElement(test1, 3));
+
+        int[] test2 = {0, 1, 2, 2, 3, 0, 4, 2};
+        // 0, 1, 2, 3, 0, 4, 2, 2
+        System.out.println(removeElement(test2, 2));
+    }
+}
