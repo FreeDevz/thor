0ff9cc88ce9dcb6fe551e2c700024f3861409fb9
androwijaya <androwijaya@gmail.com>
2025-09-06 22:01:35 +1000

    added MyClimbingStairs.java and MyFibonacciSequence.java to train on bottom-up dynamic programming.

---
+++ app/src/main/java/io/awijaya/algo/MyClimbingStairs.java
@@ -0,0 +1,34 @@
+package io.awijaya.algo;
+
+public class MyClimbingStairs {
+    public static int climbStairs(int n) {
+        if (n <= 2) {
+            return n;
+        }
+
+        int prev2 = 1; // ways to reach step 1
+        int prev1 = 2; // ways to reach step 2
+
+        for (int i = 3; i <= n; i++) {
+            int current = prev1 + prev2;
+            prev2 = prev1;
+            prev1 = current;
+        }
+
+        return prev1;
+    }
+
+    public static void main(String[] args) {
+        int test = 2;
+        System.out.println(climbStairs(test));
+
+        int test2 = 3;
+        System.out.println(climbStairs(test2));
+
+        int test3 = 4;
+        System.out.println(climbStairs(test3)); // 5
+
+        int test4 = 5;
+        System.out.println(climbStairs(test4)); // 8
+    }
+}
---
+++ app/src/main/java/io/awijaya/algo/MyFibonacciSequence.java
@@ -0,0 +1,43 @@
+package io.awijaya.algo;
+
+public class MyFibonacciSequence {
+
+    public static void printFibo(int times) {
+
+        int firstFibo = 0;
+        int secondFibo = 1;
+        System.out.println(firstFibo);
+        System.out.println(secondFibo);
+
+        for (int i = 2; i < times; i++) {
+            int sum = firstFibo + secondFibo;
+            firstFibo = secondFibo;
+            secondFibo = sum;
+            System.out.println(sum);
+        }
+    }
+
+    public static void printFiboRecursive(int times) {
+        int firstFibo = 0;
+        int secondFibo = 1;
+        System.out.println(firstFibo);
+        System.out.println(secondFibo);
+
+        printFiboRecursive(firstFibo, secondFibo, times - 2);
+    }
+
+    public static void printFiboRecursive(int firstFibo, int secondFibo, int times) {
+        if (times > 0) {
+            int sum = firstFibo + secondFibo;
+            System.out.println(sum);
+
+            printFiboRecursive(secondFibo, sum, times - 1);
+        }
+    }
+
+
+    public static void main(String[] args) {
+//        printFibo(10); // 0, 1, 1, 2, 3, 5, 8, 13, 21, 34
+        printFiboRecursive(10);
+    }
+}
